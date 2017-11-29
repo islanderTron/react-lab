675841455926619e6bfc13ce221b93d7a2ca9777
Paul Uncangco <pju93@hotmail.com>
2017-11-29 14:40:36 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+import { setInterval } from 'timers';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        {/* <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> */}
+      </div>
+    );
+    // return React.createElement('div', {className:'App'}, '',
+    //   React.createElement('h1', null, 'Hi I\'m a React App!')
+    // );
+  }
+}
+
+class HelloMessage extends Component { 
+  render() {
+    return (
+      <div>
+        Hello {this.props.message}
+      </div>
+    );
+  }
+}
+
+// const element = <h1>testing</h1>;
+
+// ReactDOM.render(
+//   <HelloMessage message= {element} />, document.getElementById('test')
+// );
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('asdf')
+);
+
+function tick() {
+  const element = (
+    <div>
+      <p>It is {new Date().toLocaleTimeString()}</p>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('time'));
+}
+setInterval(tick,1000);
+
+class Test extends Component {
+  render() {
+      return (
+          <div className="App">
+              <h1>Hi, I'm a React App</h1>
+              <p>It's really working!</p>
+          </div>
+      )
+  };
+}
+
+ReactDOM.render(<Test/>, document.getElementById('asdf'));
+export default App;
