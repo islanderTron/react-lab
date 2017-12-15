84a814e00a5181bbdb56b3b9d7dbbbfee962e734
Paul Uncangco <pju93@hotmail.com>
2017-12-15 09:47:33 -0500

    Understading about the state and new ES6,

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom';
-import { setInterval } from 'timers';
+import { setInterval, clearInterval } from 'timers';
 import Person from './Person/Person'; // import Person.js from Person/Person.js
 
 class App extends Component {
@@ -20,19 +20,19 @@ class App extends Component {
     );
     // return React.createElement('div', {className:'App'}, '',
     //   React.createElement('h1', null, 'Hi I\'m a React App!')
-    // );
+    // );``
   }
 }
 
-class HelloMessage extends Component { 
-  render() {
-    return (
-      <div>
-        Hello {this.props.message}
-      </div>
-    );
-  }
-}
+// class HelloMessage extends Component { 
+//   render() {
+//     return (
+//       <div>
+//         Hello {this.props.message}
+//       </div>
+//     );
+//   }
+// }
 
 // const element = <h1>testing</h1>;
 
@@ -40,10 +40,10 @@ class HelloMessage extends Component {
 //   <HelloMessage message= {element} />, document.getElementById('test')
 // );
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('asdf')
-);
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('asdf')
+// );
 
 // function tick() {
 //   const element = (
@@ -82,14 +82,81 @@ class Test extends Component {
           <div className="App">
               <h1>Hi, I'm a React App</h1>
               <p>It's really working!</p>
-              <button onClick={this.switchNameHandler} >Switch Name</button>
-              <Person name={this.state.persons[0].name}>My hobbies: programming!</Person>
-              <Person name={this.state.persons[1].name}/>
-              <Person name={this.state.persons[2].name}/>
+              <button onClick={this.switchNameHandler}>Switch Name</button>
+              
+              <Person 
+                name={this.state.persons[0].name}>My hobbies: programming!</Person>
+              
+              <Person 
+                name={this.state.persons[1].name}/>
+              
+              <Person 
+                name={this.state.persons[2].name}/>
           </div>
       )
   };
 }
 
-ReactDOM.render(<Test/>, document.getElementById('asdf'));
+// Without JSX - I don't like this. 
+// const element = React.createElement(
+//   'div',
+//   {className:'container'},
+//   'Hello world'
+// );
+
+// let x = function () {
+//   return "Hello world";
+// }
+
+// x;
+// console.log(x);
+// console.log(element);
+
+// ReactDOM.render(<Test/>, document.getElementById('asdf'));
+
+// Class components should always call the base construtor with props
+class PracticeState extends Component {
+  // Class constructor - special method for creating and initalizing an obj created with a class. Only 1 special method with the name "contructor" in a class. 
+  constructor(props){
+    super(props);
+    // State is similar to props, but it is private and fully controlled by the component.
+    this.state = {date: new Date()};
+  }
+  // Adding Lifecycle methods to a class
+
+  // Set up a timer whenever the Clock is rendered to the DOM for the first time.
+  // Hook run after the component output has been rendered to the DOM.
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000 );
+  }
+
+  // Clear that timer whenver the DOM produced by the Clock is removed.
+  componentDidUnMount() {
+    clearInterval(this.timerID);
+  }
+
+  // To schedule updates to the component local state
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Hello, world</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <PracticeState />,
+  document.getElementById('time')
+);
+
 export default App;
